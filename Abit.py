e8de88802920f8f7579f2a83e65be07e5a9982af
drkoster <drkoster@hotmail.com>
2024-05-22 20:03:43 +0200

    v0.0.3 - Fix and help
    Improved argument error handling and help
    
    [FIX] Prefixing hash algorithm to output filename

--- Abit.py
+++ Abit.py
@@ -1,9 +1,8 @@
 import os
-#import sys
+import sys
 import hashlib
 import argparse
 import __version__
-#from pathlib import Path
 from tqdm import tqdm
 
 # Buffered file reader method
@@ -44,29 +43,39 @@ def verify_file_hashes(input_file, algorithm):
                 if str(get_hash_from_file) != str(hashes[filepath]).rstrip():
                     print(f'Mismatch for {filepath}: calculated hash={get_hash_from_file} but stored hash={hashes[filepath]}')
             else:
-                print(f'File not found in hashtable: {filepath}')
+                print(f'File not found in input file, recommend to create new file for inculsion: {filepath}')
 
 def main():
     print("A-Bit version:", __version__.__version__)
     parser = argparse.ArgumentParser(description="Anti Bit-rot Indexing Tool")
-    parser.add_argument("--create", action='store_true', help="Create hashes for files")
-    parser.add_argument("--verify", action='store_true', help="Verify hashes of files using input file")
+    parser.add_argument("--create", action='store_true', help="Create hashes for files, requires --output")
+    parser.add_argument("--verify", action='store_true', help="Verify hashes of files using input file, requires --input")
     parser.add_argument("--algorithm", type=str, default='md5', choices=['md5', 'sha1', 'sha256', 'sha3_256'], help="Hash algorithm to use (default: md5)")
     parser.add_argument("--input", type=str, help="Input file containing hashes for verification")
     parser.add_argument("--output", type=str, default='hashes.out', help="Output file path or leave empty to print results (default: hashes.out)")
+    parser.add_argument("--starting_dir", type=str, default='.', help="The base directory to work from, includes subdirectories (default: .)")
 
     args = parser.parse_args()
 
     if not (args.create or args.verify):
-        raise ValueError("Either --create or --verify must be specified")
+        print("Either --create or --verify must be specified")
+        parser.print_help(sys.stderr)
+        return # stop application, cannot continue
+        
     elif not (args.input or args.output):
-        raise ValueError("Missing input or output file")   
+        if args.create:
+            print("Missing --output argument")
+        else:
+            print("Missing --input argument")
+        parser.print_help(sys.stderr)
+        return # stop application, cannot continue
 
     algorithm_name = args.algorithm
     input_filename = args.input
-    output_filename = os.path.join(os.getcwd(), args.output) if args.output else None
 
     if args.create:
+        target_out = algorithm_name + "_" +args.output
+        output_filename = os.path.join(os.getcwd(), target_out) if args.output else None
         create_file_hashes(output_filename, algorithm_name)
     elif args.verify:
         verify_file_hashes(input_filename, algorithm_name)
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Detect file corruption and bitrot
 
 ## Description
-Luxuries like ZFS or BTRFS are not always available. Therefore i wrote a python script that will hash all files on disk and save the path with hash to a seperate file. This file can be included onto the same media (CD, DVD or USB drive) or stored somewhere else for later verifying if the contents are still the same. 
+Luxuries like ZFS or BTRFS are not always available. Therefore as an excercise I wrote a python script that will hash all files on disk and save the path with hash to a seperate file. This file can be included onto the same media (CD, DVD or USB drive) or stored somewhere else for later verifying if the contents are still the same. 
 
 The output filename will contain the chosen hash algorithm to be given when performing verification.
 
@@ -16,6 +16,7 @@ The script supports the following arguments:
       - --algorithm             : Specify the hash algorithm to use (default: md5). Options are 'md5', 'sha1', 'sha256', and 'sha3_256'.
       - --input                 : Specify the file that contains hashes and filepaths that you wish to verify
       - --output                : Specifies the path of the output file where hashes will be written or read from.
+      - --help                  : Print help description
 
 Then execute the script by running: `python Abit.py [--create | --verify] [--algorithm md5|sha1|sha256|sha3_256] [--output output_filename]`
 
@@ -26,7 +27,7 @@ The following features might be developed, on a rainy day, someday :)
 - [ ] Option to use only filenames, making paths absolete 
 - [ ] Write results to log file
 - [ ] Threading for SSD's
-- [ ] Improved arguments error checking and help
+- [x] Improved arguments error checking and help
 - [ ] Config file for default behaviour
 
 ## Authors
@@ -36,6 +37,10 @@ Dennis Koster
 Inspired by FreeFileSync - https://freefilesync.org/
 
 ## Changelog
+
+## [0.0.3] Minor improvements
+- Added better argument handling and description
+- [FIX] Missing algorithm appending to the output file
 
 ## [0.0.2] Creature comforts
 - Added Todo's to the readme
